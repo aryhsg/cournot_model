2f5907da6e2a5f7093f2451bde367654b33c52c3
aryhsg <yangchingyu0818@gmail.com>
2023-12-20 22:10:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simple_Cournot_model',
-    version='1.0.0',
+    version='1.0.1',
     author='Yang, Ching-Yu',
     author_email='kevin81815@email.com',
     description='Test',
