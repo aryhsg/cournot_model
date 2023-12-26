cb75622d8dc8ac8f0dd1cec5cc97d776f2cd8203
aryhsg <yangchingyu0818@gmail.com>
2023-12-26 18:02:07 +0800

    Update cournot.py

--- simple_Cournot_model/cournot.py
+++ simple_Cournot_model/cournot.py
@@ -20,7 +20,7 @@ class Cournot:
                 self.pf.append(pi)
         else:
             self.profit_list_generated = False
-            self.profit_list_generated = []
+            self.pf = []
             for i in range(len(self.firm)):
                 pi = self.P_inverse * self.firm[i].q - self.firm[i].cost()
                 self.pf.append(pi)
