719143263d41e6644468c971977f37fd003be572
aryhsg <yangchingyu0818@gmail.com>
2023-12-26 16:46:05 +0800

    modify init

--- simple_Cournot_model/cournot.py
+++ simple_Cournot_model/cournot.py
@@ -2,16 +2,6 @@ from sympy import *
 import numpy as np
 from matplotlib import pyplot as plt
 
-class Firm:
-    def __init__(self, fix_cost, marginal_cost, q):
-        self.fix_cost = fix_cost
-        self.marginal_cost = marginal_cost
-        self.q = symbols(q)
-
-    def cost(self):
-        cost_func = self.fix_cost + self.marginal_cost * self.q
-        return cost_func
-
 class Cournot:
     def __init__(self, firm: list, p_intercept, p_slope):
         self.firm = firm
--- simple_Cournot_model/firm.py
+++ simple_Cournot_model/firm.py
@@ -1,7 +1,7 @@
 from sympy import *
 
 class Firm:
-    def __init__(self, fix_cost, marginal_cost, q):
+    def __init__(self, fix_cost:int , marginal_cost:int , q: str):
         self.fix_cost = fix_cost
         self.marginal_cost = marginal_cost
         self.q = symbols(q)
